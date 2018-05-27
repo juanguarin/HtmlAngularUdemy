1302caad1d6505b19c619d74d9eb044bee110677
Juan Carlos Guarin Walteros <jcgwalteros01@gmail.com>
2018-05-27 00:05:38 -0500

    Agregando componente nuevo.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+/*import { Component, OnInit } from '@angular/core'; SE ELIMINA*/
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'/*, SE ELIMINA
+  styleUrls: ['./header.component.css']*/
+})
+/*export class HeaderComponent implements OnInit { SE ELIMINA*/
+export class HeaderComponent {
+
+  constructor() { }
+
+  /*ngOnInit() {
+  } SE ELIMINA */
+
+}
