6e73020a0b85bf9e8edf83c75bdfe5d387e1cc39
Juan Carlos Guarin Walteros <jcgwalteros01@gmail.com>
2018-06-12 21:23:29 -0500

    Se agrego funcionalidad para filtrar los produtos usando promesas.

--- src/app/services/prodcutos.service.ts
+++ src/app/services/prodcutos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Injectable()
 export class ProdcutosService {
@@ -19,12 +21,42 @@ export class ProdcutosService {
 public buscar_producto( termino: string) {
   console.log( 'Buscando productos' );
   console.log( this.productos.length );
+
+  if (this.productos.length === 0) {
+
+    this.cargar_producto().then( () => {
+            // En este moemnto se sabe si ya termino la carga. Gracias a la promesa del metodo cargar_producto.
+      this.filtrar_productos(termino); // No se ejecuta hasta no terminar cargar_producto.
+    });
+
+  } else {
+
+    this.filtrar_productos(termino);
+
+  }
+
+
+}
+/**
+ * Metodo para filtrar la información.
+ */
+private filtrar_productos(termino: string) {
+
+  this.productos_filtrado = [];
+
+  termino = termino.toLowerCase(); // Se vuelve minuscula para poder facilitar la busqueda ya que javaScript
+                                         // es case sensitive.
+
   this.productos.forEach( prod => {
-      console.log( prod );
+      // console.log( prod );
+      // Se usa toLowerCase para volver los valores de la propiedad en minuscula.
+    if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0 ) {
+        this.productos_filtrado.push( prod );
+        // console.log(prod);
+      }
   });
 
 }
-
   /**
    * Metodo para cargar los productos de la pagian del portafolio.
    */
@@ -32,19 +64,23 @@ public buscar_producto( termino: string) {
 
     this.cargando = true;
 
-    if (this.productos.length === 0) {
-
+    let promesa = new Promise(( resolve, reject ) => { /* Se agrega el maenjo de promesas.
+       Que abarca el manejo del subscribe para obtener la información.*/
       this.http.get('https://angularhtmlweb.firebaseio.com/productos_idx.json')
       .subscribe( res => {  // console.log(res.json());
       /*Se agrega la funcion setTimeout para que la carga se demore 1,5 segundos. */
-      setTimeout(() => {
+      // setTimeout(() => {
         this.productos = res.json();
         this.cargando = false;
-      }, 1500);
+        resolve();
+      // }, 1500);
+
+    });
+    // if (this.productos.length === 0) {
 
       });
-
-    }
+    return promesa;
+    // }
 
   }
 
